939a76733dc381840af18d4fa1e43de7f1cdc1af
Lucas Barga <lucasmpbarga@gmail.com>
2023-01-04 17:10:21 -0300

    notes app adding notes

--- section_04/notes-app/notes.js
+++ section_04/notes-app/notes.js
@@ -1,3 +1,42 @@
-const getNotes = () => "Your notes...";
+const { constants } = require("buffer");
+const fs = require("fs");
 
-module.exports = getNotes;
+const getNotes = () => {
+  return "Your notes...";
+};
+
+const loadNotes = () => {
+  try {
+    const notesBuffer = fs.readFileSync("notes.json");
+    const notesJson = notesBuffer.toString();
+    return JSON.parse(notesJson);
+  } catch (e) {
+    return [];
+  }
+};
+
+const saveNotes = (notes) => {
+  const notesJson = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", notesJson);
+};
+
+const addNote = (title, body) => {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title,
+      body,
+    });
+
+    saveNotes(notes);
+  } else {
+    console.log("Note title taken!");
+  }
+};
+
+module.exports = {
+  getNotes,
+  addNote,
+};
